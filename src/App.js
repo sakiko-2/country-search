2c3bee4c1bc6ebb0d4adba86feeb04687c8b67be
saki <16546851+sakiko-2@users.noreply.github.com>
2022-08-03 23:01:54 +1200

    Set up prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
