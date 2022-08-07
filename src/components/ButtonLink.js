c2312eb5af60d2597ee9de52e9496f57b5519d9d
saki <16546851+sakiko-2@users.noreply.github.com>
2022-08-07 17:47:18 +1200

    Improve link size for small screen

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -2,13 +2,17 @@ import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
 const StyledLink = styled(Link)`
-  min-width: 7rem;
+  min-width: 5.5rem;
+
+  @media (min-width: 640px) {
+    min-width: 7rem;
+  }
 `
 
 const ButtonLink = ({item, name}) => (
   <StyledLink
     to={`/country/${item}`}
-    className="inline-block text-center rounded border px-4 py-1 m-0.5 bg-white drop-shadow-md"
+    className="inline-block text-center rounded border px-4 py-1 m-0.5 bg-white drop-shadow-md text-sm sm:text-base"
   >
     {name(item)}
   </StyledLink>
