dd9965a809b20034e4b39ec5fc92614f88e86261
saki <16546851+sakiko-2@users.noreply.github.com>
2022-08-12 14:26:46 +1200

    Add footer

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,5 +1,6 @@
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Layout = () => (
   <div className="flex min-h-screen flex-col bg-[color:var(--very-light-gray)] text-[color:var(--mostly-black-blue)] dark:bg-[color:var(--very-dark-blue)] dark:text-white">
@@ -7,6 +8,7 @@ const Layout = () => (
     <div className="container flex flex-1 flex-col px-5 py-6 sm:mx-auto">
       <Outlet />
     </div>
+    <Footer />
   </div>
 )
 
