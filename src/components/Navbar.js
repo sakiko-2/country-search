fb7789c64605f06996ec36985eb1ca01d2fa7242
saki <16546851+sakiko-2@users.noreply.github.com>
2022-08-22 20:53:24 +1200

    Fix for keyboard focus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,17 @@ const Navbar = () => {
 
   return (
     <nav className="bg-[color:var(--light-primary-color)] shadow-md dark:bg-[color:var(--dark-primary-color)] dark:shadow-black">
-      <div className="container flex items-center justify-between px-3 py-5 md:mx-auto">
-        <Link to="/" className="font-extrabold">
+      <div className="container flex items-center justify-between px-3 py-4 md:mx-auto">
+        <Link to="/" className="rounded p-1 font-extrabold">
           Where in the world?
         </Link>
-        <div
-          className="jusify-between flex cursor-pointer items-center"
+        <button
+          className="jusify-between flex items-center rounded p-1"
           onClick={() => setDark(!dark)}
         >
           <MoonIcon className="mr-2 h-5 w-5" />
           <span>Dark Mode</span>
-        </div>
+        </button>
       </div>
     </nav>
   )
